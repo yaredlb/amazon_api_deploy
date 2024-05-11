9402879e3ba8ecda733c50c4e3c2a251b2daac08
yaredlb <yaredthekop@gmail.com>
2024-05-11 14:05:24 -0500

    initial commit for amazon backend deployment

---
+++ main.jsx
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    messsage: "Success !",
+  });
+});
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    // console.log("payment received", total);
+    // res.send(total);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    // console.log(paymentIntent);
+
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "total must be greater than 0",
+    });
+  }
+});
+
+app.listen(5000, (err) => {
+  if (err) throw err;
+  console.log("Amazon server running on PORT : 5000, http://localhost:5000");
+});
